e6790cd6cc41ec020ff0e93edcf3c4053c600bee
Tapes <T@Tapes.local>
2016-04-03 22:34:46 +0300

    viikko 2

--- viikko2/Ohtu-NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
+++ viikko2/Ohtu-NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
@@ -1,8 +1,10 @@
 package ohtuesimerkki;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Statistics stats = new Statistics();
+        Statistics stats = new Statistics( new PlayerReader("http://nhlstats-2013-14.herokuapp.com/players.txt") );
           
         System.out.println("Philadelphia Flyers");
         for (Player player : stats.team("PHI") ) {
---
+++ viikko2/Ohtu-NhlStatistics1/src/test/java/StatisticsTest.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import ohtuesimerkki.Player;
+import ohtuesimerkki.PlayerReader;
+import ohtuesimerkki.Statistics;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StatisticsTest {
+    
+    Statistics stats;
+    PlayerReader readerStub = new PlayerReader("asd") {
+
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<Player>();
+
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+
+            return players;
+        }
+    };
+    
+    public StatisticsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void searchTest() {
+        stats = new Statistics(readerStub);
+        Player p = stats.search("Semenko");
+        List l = readerStub.getPlayers();
+        assertEquals(l.get(0), p);
+    }
+}
