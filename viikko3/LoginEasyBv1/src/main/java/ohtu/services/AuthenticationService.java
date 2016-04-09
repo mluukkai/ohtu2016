654ba1c39db17b04e92d4e34cfcd6d58206ba690
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2016-04-09 18:33:54 +0300

    tehtävään 4 asti tehty

--- viikko3/LoginEasyBv1/src/main/java/ohtu/services/AuthenticationService.java
+++ viikko3/LoginEasyBv1/src/main/java/ohtu/services/AuthenticationService.java
@@ -40,7 +40,11 @@ public class AuthenticationService {
 
     private boolean invalid(String username, String password) {
         // validity check of username and password
-
+        if (username.length() < 3) return true;
+        if (username.matches(".*[^a-z].*")) return true;
+        if (password.length() < 8) return true;
+        if (!password.matches(".*[^a-zA-Z].*")) return true;
+        
         return false;
     }
 }
