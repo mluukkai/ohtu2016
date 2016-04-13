f00192ca794c5246656134e78f4f779a8be3d9ef
Esa Potkonen <essi.potkonen@gmail.com>
2016-04-13 21:09:49 +0300

    Viikko 4 tehtävät mutta ei eka, koska se meni jonnekkin remoten syövereihin

--- PotkonenEsa/viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ PotkonenEsa/viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -7,6 +7,18 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
     public static void main(String[] args) {
+        Tester t = new Tester();
+        t.login("pekka", "akkep");
+        t.login("pekka", "akkepx");
+        t.login("pekkax", "akkep");
+        WebDriver driver = new HtmlUnitDriver();
+        t.addUser(driver, "esaxxx", "12345asdfg");
+        t.login("esaxxx", "12345asdfg");
+        driver = new HtmlUnitDriver();
+        t.addUser(driver, "esaxxxx", "12345asdfg");
+        t.logout(driver);
+    }
+    private void login(String user, String passw) {
         WebDriver driver = new HtmlUnitDriver();
 
         driver.get("http://localhost:8090");
@@ -18,9 +30,9 @@ public class Tester {
         
         System.out.println( driver.getPageSource() );
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys(user);
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
+        element.sendKeys(passw);
         element = driver.findElement(By.name("login"));
         element.submit();
         
@@ -28,4 +40,40 @@ public class Tester {
         System.out.println( driver.getPageSource() );
         
     }
-}
+    private void addUser(WebDriver driver,String user, String passw) {
+ 
+        driver.get("http://localhost:8090");
+        System.out.println( driver.getPageSource() );
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+        
+        System.out.println("==Register");
+        
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.name("username"));
+        element.sendKeys(user);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(passw);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(passw);
+        
+        element = driver.findElement(By.name("add"));
+        element.submit();
+        
+        System.out.println("==Add");
+        System.out.println( driver.getPageSource() );
+     
+        element = driver.findElement(By.linkText("continue to application mainpage"));       
+        element.click(); 
+
+        System.out.println("==Continue");
+        System.out.println( driver.getPageSource() );
+    }
+    private void logout(WebDriver driver) {
+        System.out.println( driver.getPageSource() );
+        WebElement element = driver.findElement(By.linkText("logout"));       
+        element.click(); 
+        System.out.println("==Logout");
+        System.out.println( driver.getPageSource() );
+    }
+ }
