94621fc61d0c81c96651a79a4c3838a127680b44
Oskar Koli <koli.oskar@gmail.com>
2016-04-16 13:31:49 +0300

    Viikko 4 t2

--- viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,7 +1,41 @@
 package ohtu;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.io.IOUtils;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String studentNr = "014168501";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "http://ohtustats2016.herokuapp.com/students/"+studentNr+"/submissions";
+
+        HttpClient client = new HttpClient();
+        GetMethod method = new GetMethod(url);
+        client.executeMethod(method);
+
+        InputStream stream =  method.getResponseBodyAsStream();
+
+        String bodyText = IOUtils.toString(stream);
+        
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        int hourSum = 0;
+        int tehavaSum = 0;
+        System.out.println("opiskelijanumero " + studentNr);
+        for (Submission submission : subs) {
+            System.out.println("   " + submission);
+            hourSum += submission.getHours();
+            tehavaSum += submission.tehdytTehtavatSum();
+        }
+        System.out.println("yhteensä: " + tehavaSum + " tehtävää " + hourSum + " tuntia");
+
     }
 }
--- viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -2,7 +2,32 @@ package ohtu;
 
 public class Submission {
     private String student_number;
-
+    private String last_name;
+    private String first_name;
+    private int week;
+    private int hours;
+    private boolean a1;
+    private boolean a2;
+    private boolean a3;
+    private boolean a4;
+    private boolean a5;
+    private boolean a6;
+    private boolean a7;
+    private boolean a8;
+    private boolean a9;
+    private boolean a10;
+    private boolean a11;
+    private boolean a12;
+    private boolean a13;
+    private boolean a14;
+    private boolean a15;
+    private boolean a16;
+    private boolean a17;
+    private boolean a18;
+    private boolean a19;
+    private boolean a20;
+    private boolean a21;
+    
     public String getStudent_number() {
         return student_number;
     }
@@ -10,10 +35,56 @@ public class Submission {
     public void setStudent_number(String student_number) {
         this.student_number = student_number;
     }
-
+    
     @Override
     public String toString() {
-        return student_number;
+        return "viikko " + this.week + " tehtyjä tehtäviä yhteensä: " + this.tehdytTehtavatSum() +
+                ", aikaa kului " + this.hours + " tuntia, tehdyt tehtävät: " + this.tehdytTehtavatStr();
     }
     
+    int[] tehdytTehtavatArr() {
+        return new int[] {
+            (a1 ? 1 : 0),
+            (a2 ? 1 : 0),
+            (a3 ? 1 : 0),
+            (a4 ? 1 : 0),
+            (a5 ? 1 : 0),
+            (a6 ? 1 : 0),
+            (a7 ? 1 : 0),
+            (a8 ? 1 : 0),
+            (a9 ? 1 : 0),
+            (a10 ? 1 : 0),
+            (a11 ? 1 : 0),
+            (a12 ? 1 : 0),
+            (a13 ? 1 : 0),
+            (a14 ? 1 : 0),
+            (a15 ? 1 : 0),
+            (a16 ? 1 : 0),
+            (a17 ? 1 : 0),
+            (a18 ? 1 : 0),
+            (a19 ? 1 : 0),
+            (a20 ? 1 : 0),
+            (a21 ? 1 : 0)
+        };
+    }
+    
+    String tehdytTehtavatStr() {
+        String ret = "";
+        int i = 0;
+        for (int a : this.tehdytTehtavatArr()) {
+            i++;
+            if (a == 1) ret += i + " ";
+        }
+        return ret;
+    }
+    
+    public int tehdytTehtavatSum() {
+        int sum = 0;
+        for (int a : this.tehdytTehtavatArr()) sum += a;
+        return sum;
+    }
+    
+    public int getHours() {
+        return this.hours;
+    }
 }
