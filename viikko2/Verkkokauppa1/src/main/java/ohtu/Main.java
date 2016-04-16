1c97b0790dda21125fc537f750bd3025b3346552
Oskar Koli <koli.oskar@gmail.com>
2016-04-16 13:02:55 +0300

    Added Viikko4 folder

--- viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -12,30 +12,30 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
-        
         ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
-        
         Kauppa kauppa = (Kauppa) ctx.getBean("kauppa");
         Kirjanpito kirjanpito = (Kirjanpito) ctx.getBean("kirjanpito");
-        // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
+        Main.fn1(kauppa, kirjanpito);   
+    }
+    
+    static void fn1(Kauppa kauppa, Kirjanpito kirjanpito) {
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);
         kauppa.lisaaKoriin(3);
         kauppa.lisaaKoriin(3);
+        Main.fn2(kauppa);
+        for (String tapahtuma : kirjanpito.getTapahtumat()) {
+            System.out.println(tapahtuma);
+        }
+    }
+    
+    static void fn2(Kauppa kauppa) {
         kauppa.poistaKorista(1);
         kauppa.tilimaksu("Pekka Mikkola", "1234-12345");
-
-        // seuraava asiakas
         kauppa.aloitaAsiointi();
         for (int i = 0; i < 24; i++) {
             kauppa.lisaaKoriin(5);
         }
-
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
-
-        // kirjanpito
-        for (String tapahtuma : kirjanpito.getTapahtumat()) {
-            System.out.println(tapahtuma);
-        }
     }
 }
