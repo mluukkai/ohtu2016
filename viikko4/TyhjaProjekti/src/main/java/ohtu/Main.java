ecadf530fa7c26b002cfde1f948ef07e533a2944
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-04-16 15:45:59 +0300

    viikko 4, tehtävät 1-3

--- viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,7 +1,51 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.io.IOUtils;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String studentNr = "014423783";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "http://ohtustats2016.herokuapp.com/students/"+studentNr+"/submissions";
+
+        HttpClient client = new HttpClient();
+        GetMethod method = new GetMethod(url);
+        client.executeMethod(method);
+
+        InputStream stream =  method.getResponseBodyAsStream();
+
+        String bodyText = IOUtils.toString(stream);
+
+        //System.out.println("json-muotoinen data:");
+        //System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        
+        int all_hours = 0;
+        int all_assignments = 0;
+        
+        for (Submission submission : subs) {
+            submission.assignments();
+            all_hours += submission.getHours();
+            all_assignments += submission.getAssignmentsAmount();
+        }
+
+        System.out.println("opiskelijanumero: " + studentNr);
+        System.out.println();
+        for (Submission submission : subs) {
+            System.out.println(submission);
+        }
+        System.out.println();
+        System.out.println("yhteensä: " + all_assignments + " tehtävää, " + all_hours + " tuntia.");
     }
 }
--- viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -1,10 +1,35 @@
 package ohtu;
 
+import java.util.ArrayList;
+
 public class Submission {
     private String student_number;
+    private boolean a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a19, a20, a21;
+    private int hours;
+    private int week;
+    private ArrayList<Integer> assignments;
+    
+    public void assignments() {
+        boolean[] assignment = 
+        { false, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a19, a20, a21 };
+        
+        assignments = new ArrayList<Integer>();
+        
+        for (int i = 0; i < 21; i++) {
+            if (assignment[i]) assignments.add(i);
+        }
+    }
 
     public String getStudent_number() {
         return student_number;
+    }
+    
+    public int getHours() {
+        return hours;
+    }
+    
+    public int getAssignmentsAmount() {
+        return assignments.size();
     }
 
     public void setStudent_number(String student_number) {
@@ -13,7 +38,8 @@ public class Submission {
 
     @Override
     public String toString() {
-        return student_number;
+        return "viikko " + week + ": tehtyjä tehtäviä yhteensä: " + assignments.size() + ", aikaa kului: " +
+                hours + " tuntia, tehdyt tehtävät: " + assignments.toString();
     }
     
 }
