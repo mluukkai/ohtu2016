dfbb8f3e95831327eb8b44d6064d248ca83678d0
Joni Hanski <joni.hanski@helsinki.fi>
2016-04-17 22:47:49 +0300

    eka commit

--- viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -2,6 +2,9 @@ package ohtu;
 
 public class Submission {
     private String student_number;
+    private String week;
+    private String points;
+    private String hours;
 
     public String getStudent_number() {
         return student_number;
@@ -11,9 +14,35 @@ public class Submission {
         this.student_number = student_number;
     }
 
+    public String getWeek() {
+        return week;
+    }
+
+    public void setWeek(String week) {
+        this.week = week;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    public void setHours(String hours) {
+        this.hours = hours;
+    }
+    
+    
+
     @Override
     public String toString() {
-        return student_number;
+        return "Viikko " + week + ": Tehtyjä tehtäviä " + points + ", käytettyjä tunteja " + hours + "\n";
     }
     
 }
