6b411e5a05d6edb529365f6e9d124b89c50de159
Joni Hanski <joni.hanski@helsinki.fi>
2016-04-17 23:48:15 +0300

    Vika

--- viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -3,25 +3,29 @@ package ohtu;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
     public static void main(String[] args) {
-        WebDriver driver = new HtmlUnitDriver();
-
-        driver.get("http://localhost:8090");
+        //WebDriver driver = new HtmlUnitDriver();
+        WebDriver driver = new FirefoxDriver();
+        
+        driver.get("http://localhost:8080");
         System.out.println( driver.getPageSource() );
-        WebElement element = driver.findElement(By.linkText("login"));       
+        WebElement element = driver.findElement(By.linkText("register new user"));       
         element.click(); 
         
         System.out.println("==");
         
         System.out.println( driver.getPageSource() );
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("pekka2");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
+        element.sendKeys("akkep222");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("akkep222");
+        element = driver.findElement(By.name("add"));
         element.submit();
         
         System.out.println("==");
