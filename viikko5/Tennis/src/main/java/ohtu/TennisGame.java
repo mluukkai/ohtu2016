bc29491ba20f0d5379b3a1b3b779ba5bcc12fd2e
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-04-22 19:31:58 +0300

    tenniksen refarktorointi

--- viikko5/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko5/Tennis/src/main/java/ohtu/TennisGame.java
@@ -2,8 +2,8 @@ package ohtu;
 
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1_scores = 0;
+    private int player2_scores = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,67 +13,64 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name)) player1_scores += 1;
+        else player2_scores += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (player1_scores == player2_scores) score = tied(score);       
+        else if (player1_scores >= 4 || player2_scores >= 4) score = advantage(score);
+        else score = miscSituation(score, player1_scores) + "-" + miscSituation(score, player2_scores);
+ 
+        return score;
+    }
+    
+    public String tied(String score) {
+         switch (player1_scores) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break; 
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        return score;
+    }
+    
+    public String advantage(String score) {
+        int scoreSubtraction = player1_scores - player2_scores;
+        if (scoreSubtraction == 1) score = "Advantage player1";
+        else if (scoreSubtraction == -1) score = "Advantage player2";
+        else if (scoreSubtraction >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        
+        return score;
+    }
+    
+    public String miscSituation(String score, int player_scores) {
+        switch(player_scores) {
+            case 0:
+                score = "Love";
+                break;
+            case 1:
+                score = "Fifteen";
+                break;
+            case 2:
+                score = "Thirty";
+                break;
+            case 3:
+                score = "Forty";
+                break;
         }
         return score;
     }
