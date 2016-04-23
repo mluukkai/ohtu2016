67cb8524e5977a4c524a9688a4d9f7e123ffd86b
Oskar Koli <koli.oskar@gmail.com>
2016-04-23 11:38:20 +0300

    Viikko 5 tehtävä 2

--- viikko5/LyyrakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
+++ viikko5/LyyrakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
@@ -45,4 +45,16 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void kortilleVoiLadataPositiivisenLuvun() {
+        kassa.lataa(kortti, 10);
+        verify(kortti, times(1)).lataa(10);
+    }
+    
+    @Test
+    public void kortilleEiVoiLadataNegatiivisenLuvun() {
+        kassa.lataa(kortti, -10);
+        verify(kortti, times(0)).lataa(anyInt());
+    }
 }
